24d9c03201f2102ccf1481398f30737804246330
Zsolt Ero <zsolt.ero@gmail.com>
2025-06-20 04:18:24 +0200

    work

--- vite_stylex/src/App.tsx
+++ vite_stylex/src/App.tsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as stylex from '@stylexjs/stylex'
+import { spacing } from './tokens.stylex'
+
+const styles = stylex.create({
+  base: {
+    color: 'red',
+    fontSize: spacing.xxlarge,
+  },
+})
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <div {...stylex.props(styles.base)}>Big Red if works</div>
 }
 
 export default App
--- vite_stylex/vite.config.ts
+++ vite_stylex/vite.config.ts
@@ -1,7 +1,42 @@
-import { defineConfig } from 'vite'
+import path from 'node:path'
+// @ts-ignore
+import stylex from '@stylexjs/postcss-plugin'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
+const babelConfig = {
+  presets: ['@babel/preset-typescript'],
+  plugins: [
+    [
+      '@stylexjs/babel-plugin',
+      {
+        dev: process.env.NODE_ENV === 'development',
+        test: process.env.NODE_ENV === 'test',
+        runtimeInjection: false,
+        treeshakeCompensation: true,
+        unstable_moduleResolution: {
+          type: 'commonJS',
+          rootDir: '.',
+        },
+      },
+    ],
+  ],
+}
+
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    cssMinify: false, // just to see the results
+  },
+  plugins: [react({ babel: babelConfig })],
+  css: {
+    postcss: {
+      plugins: [
+        stylex({
+          babelConfig,
+          include: [path.resolve('src/**/*.{ts,tsx}')],
+          useCSSLayers: false,
+        }),
+      ],
+    },
+  },
 })
