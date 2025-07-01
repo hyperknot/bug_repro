c26ff25ec14e1156e654d296a75bed0ca441306b
Zsolt Ero <zsolt.ero@gmail.com>
2025-07-01 02:38:36 +0200

    work

--- vite_unocss/app/vite.config.ts
+++ vite_unocss/app/vite.config.ts
@@ -1,31 +1,5 @@
 import path from 'node:path'
-import type { TransformOptions } from '@babel/core'
-// @ts-ignore
-import stylexPostcss from '@stylexjs/postcss-plugin'
 import { defineConfig } from 'vite'
-import solid from './vite-plugin-solid-mod'
-
-// Shared StyleX Babel configuration
-const stylexBabelConfig: TransformOptions = {
-  presets: ['@babel/preset-typescript'],
-  plugins: [
-    [
-      '@stylexjs/babel-plugin',
-      {
-        dev: process.env.NODE_ENV === 'development',
-        test: process.env.NODE_ENV === 'test',
-        runtimeInjection: false,
-        treeshakeCompensation: true,
-        unstable_moduleResolution: {
-          type: 'commonJS',
-        },
-        aliases: {
-          '@alias/*': path.resolve('../alias/*'),
-        },
-      },
-    ],
-  ],
-}
 
 export default defineConfig({
   build: {
@@ -36,28 +10,7 @@ export default defineConfig({
     alias: {
       '@alias': path.resolve('../alias'),
     },
-    dedupe: ['solid-js', '@stylexjs/stylex'],
+    dedupe: ['solid-js'],
   },
-  plugins: [
-    solid({
-      babel: stylexBabelConfig,
-    }),
-  ],
-  css: {
-    postcss: {
-      plugins: [
-        stylexPostcss({
-          babelConfig: stylexBabelConfig,
-          include: [
-            'src/**/*.{ts,tsx}',
-            './*.{ts,tsx}',
-            '../outside/**/*.{ts,tsx}',
-            '../alias/**/*.{ts,tsx}',
-            'node_modules/@repo/workspace/**/*.{ts,tsx}',
-          ],
-          useCSSLayers: true,
-        }),
-      ],
-    },
-  },
+  plugins: [],
 })
