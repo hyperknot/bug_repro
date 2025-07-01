07ea58de59620909ccdc4825e1879b7db542281d
Zsolt Ero <zsolt.ero@gmail.com>
2025-07-01 12:46:30 +0200

    work

--- stylex_solid_attr/src/App.tsx
+++ stylex_solid_attr/src/App.tsx
@@ -6,6 +6,8 @@ import { srcVars } from './vars.stylex'
 const styles = stylex.create({
   local: {
     color: 'red',
+    fontSize: '22px',
+    textDecoration: 'underline',
   },
 
   srcVar: {
@@ -15,18 +17,48 @@ const styles = stylex.create({
   projVar: {
     color: projVars.color,
   },
+
+  dynamic: (fontSize) => ({
+    fontSize,
+  }),
 })
 
 function App() {
+  const inline = { color: 'blue', fontSize: '16px', textDecoration: 'underline' }
+
   return (
     <div>
-      <div {...sx(styles.local)}>sx local value - red if works</div>
+      <div {...sx(styles.local)}>sx local value - big red if works</div>
       <div {...sx(styles.srcVar)}>sx var from ./src/ - green if works</div>
       <div {...sx(styles.projVar)}>sx var from ../ - orange if works</div>
+      <div {...sx(styles.local, inline)}>sx inline - small blue underlined if works</div>
 
       <div {...attrs(stylex.props(styles.local))}>attr local value - red if works</div>
       <div {...attrs(stylex.props(styles.srcVar))}>attr var from ./src/ - green if works</div>
       <div {...attrs(stylex.props(styles.projVar))}>attr var from ../ - orange if works</div>
+      <div {...attrs(stylex.props(styles.local, styles.dynamic('14px')))}>
+        attrs dynamic - 14px blue underlined if works
+      </div>
+
+      <div {...stylex.props(styles.local)}>props local value - red if works</div>
+      <div {...stylex.props(styles.srcVar)}>props var from ./src/ - green if works</div>
+      <div {...stylex.props(styles.projVar)}>props var from ../ - orange if works</div>
+      <div {...stylex.props(styles.local, styles.dynamic)}>
+        sx inline - small blue underlined if works
+      </div>
+
+      <div {...stylex.attrs(styles.local)}>
+        native attr (only works on 0.12) local value - red if works
+      </div>
+      <div {...stylex.attrs(styles.srcVar)}>
+        native attr (only works on 0.12) var from ./src/ - green if works
+      </div>
+      <div {...stylex.attrs(styles.projVar)}>
+        native attr (only works on 0.12) var from ../ - orange if works
+      </div>
+      <div {...stylex.attrs(styles.local, inline)}>
+        native attr (only works on 0.12) inline - small blue underlined if works
+      </div>
     </div>
   )
 }
